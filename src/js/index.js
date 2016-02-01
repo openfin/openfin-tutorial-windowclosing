631b552e28087d72049095fa140fd7c36cbf3e17
mrClapham <info@gcdesign.co.uk>
2016-02-01 15:58:32 +0000

    Now terminating all apps.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
+var _mainWin, apps = []
+
+document.addEventListener("DOMContentLoaded", function(){
+    init();
+});
+//------
+
+
 function init(){
     console.log("Dom Loaded ", this);
     try{
@@ -10,10 +18,65 @@ function init(){
 };
 
 function initWithOpenFin(){
-    alert("OpenFin is available");
     // Your OpenFin specific code to go here...
+
+    // NB it is 'Window' not 'Application' that the EventListener is being attached to
+    _mainWin = fin.desktop.Window.getCurrent()
+    _mainWin.addEventListener('close-requested', function(e) {
+        var challenge = confirm('are you sure?');
+        if (challenge == true) {
+            terminateAllApps();
+            _mainWin.close(true);
+
+        }else{
+            console.log("The confirm was false")
+        }
+    });
+//create an new app
+    initNewApp("BGCIROVolumeMatch").then(function(value){
+        console.log("________ Adding to the array ", value)
+        apps.push(value)
+    });
+// and a second - for good measure...
+    initNewApp("BGCIROVolumeMatch2").then(function(value){
+        console.log("________ Adding to the array ", value)
+        apps.push(value)
+    });
 }
 
 function initNoOpenFin(){
     alert("OpenFin is not available - you are probably running in a browser.");
+}
+
+function terminateAllApps(){
+    for(var app in apps ){
+        apps[app].terminate();
+    }
+}
+
+
+function initNewApp(uuid){
+    return new Promise(function(resolve, reject){
+        var volumeMatchApplication = new fin.desktop.Application({
+            name: "BGC IRO Volume Match",
+            uuid: uuid,
+            url: "http://localhost:3030/child.html",
+            mainWindowOptions: {
+                name: "BGC IRO Volume Match",
+                autoShow: true,
+                defaultCentered: true,
+                alwaysOnTop: true,
+                state: "minimized",
+                saveWindowState: true,
+                icon: "favicon.ico"
+            }
+        }, function () {
+            // Ensure the Volume Match application is closed when the main application is closed.
+            console.log("running");
+            volumeMatchApplication.run();
+            resolve(volumeMatchApplication)
+        });
+    })
+
+
 }
