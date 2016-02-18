2f6fc6eae7210ab990bfe34512c156748dccc74a
mrClapham <info@gcdesign.co.uk>
2016-02-18 15:17:16 +0000

    Added resize function to the child windows.

--- src/js/child.js
+++ src/js/child.js
@@ -13,5 +13,23 @@ function initChild(){
 
 initChildWithOpenFin = function(){
   _childApp = fin.desktop.Window.getCurrent();
-  _childApp.show();
+
+  _childApp.show(function() {
+    console.log("showing window then setting width and height to 1000x1000");
+      console.log("resize succeeded");
+    }, function() {
+      console.log("resize failed");
+    });
+
+  _childApp.addEventListener("restored", function (event) {
+    console.log("The window has been moved or restored");
+    _childApp.setBounds(0, 0, 1000, 1000, function () {
+
+    }, function () {
+      console.log("The registration was successful");
+    }, function (reason) {
+      console.log("failure:" + reason);
+    });
+
+  });
 };
